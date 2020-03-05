a174fc8e266a1441bcec6301bd86f71e640312a8
Administrator <rlawjdxo1212@naver.com>
2020-03-05 11:13:55 +0900

    card design

---
+++ lib/constanst.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kNumberFont = TextStyle(
+    color: Colors.white,
+    fontFamily: 'U and I',
+    fontWeight: FontWeight.bold,
+    fontSize: 25);
+
+const kTextStyle = TextStyle(
+  fontSize: 13,
+  fontWeight: FontWeight.w800,
+  color: Colors.white,
+  fontFamily: 'U and I',
+);
+
+const kInputTextStyle = TextStyle(
+  fontSize: 20,
+  color: Colors.white,
+  fontFamily: 'U and I',
+);
--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/constanst.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key key}) : super(key: key);
@@ -9,9 +10,50 @@ class MainScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  child: Column(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              MyAppbar()
+              MyAppbar(),
+              Container(
+                
+                margin: EdgeInsets.symmetric(vertical: 30),
+                height: 250,
+                
+                decoration: BoxDecoration(
+                    color: const Color(0xFF5D5D5E),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Stack(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: CardNumber(),
+                      ),
+                      Align(
+                          alignment: Alignment.topRight,
+                          child: Image.asset('images/mastercard.png')),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Text(
+                              'CARDHOLDER NAME',
+                              style: kTextStyle,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text('NAME SURFACE', style: kInputTextStyle,),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -20,17 +62,50 @@ class MainScreen extends StatelessWidget {
   }
 }
 
+class CardNumber extends StatelessWidget {
+  final itemPadding = 25.0;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(
+          '1234',
+          style: kNumberFont,
+        ),
+        SizedBox(
+          width: itemPadding,
+        ),
+        Text(
+          '1234',
+          style: kNumberFont,
+        ),
+        SizedBox(
+          width: itemPadding,
+        ),
+        Text(
+          '1234',
+          style: kNumberFont,
+        ),
+        SizedBox(
+          width: itemPadding,
+        ),
+        Text(
+          '1234',
+          style: kNumberFont,
+        )
+      ],
+    );
+  }
+}
+
 class MyAppbar extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         IconButton(
-            icon: Icon(Icons.clear, color: Colors.black),
-            onPressed: null),
+            icon: Icon(Icons.clear, color: Colors.black), onPressed: null),
         Container(
           padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
           decoration: BoxDecoration(
@@ -41,8 +116,8 @@ class MyAppbar extends StatelessWidget {
             children: <Widget>[
               Text(
                 'Scan your card   ',
-                style: TextStyle(
-                    color: Colors.black, fontWeight: FontWeight.bold),
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
               ),
               Icon(Icons.camera_alt)
             ],
